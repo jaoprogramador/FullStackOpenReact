28e42bdc8974afae68ebde930b0ed5bd5ef60232
jao <jaoprogramador@gmail.com>
2024-12-17 15:51:59 +0100

    Cambios port

--- Parte10Reactnativo/dProbaryampliarnuestraaplicacion/rate-repository-api-master/src/index.js
+++ Parte10Reactnativo/dProbaryampliarnuestraaplicacion/rate-repository-api-master/src/index.js
@@ -1,11 +1,11 @@
 import http from 'http';
 
 import logger from './utils/logger';
-import { API_PORT, APOLLO_PORT } from './config';
+import {  APOLLO_PORT } from './config';
 import createApolloServer from './apolloServer';
 import app from './app';
 
-const startServer = async () => {
+/* const startServer = async () => {
   const httpServer = http.createServer(app);
 
   const apolloServer = createApolloServer();
@@ -21,4 +21,25 @@ const startServer = async () => {
   logger.info(`Apollo Server ready at http://localhost:${APOLLO_PORT}`);
 };
 
+startServer(); */
+const startServer = async () => {
+  // Detectar el puerto del entorno o usar un valor por defecto
+  const PORT = process.env.PORT || 5000;
+
+  const httpServer = http.createServer(app);
+
+  // Configuración del servidor Apollo
+  const apolloServer = createApolloServer();
+  await apolloServer.listen({ port: APOLLO_PORT }); // Apollo puede usar un puerto diferente para GraphQL
+
+  httpServer.on('request', app.callback());
+
+  // Escuchar en el puerto detectado por Render
+  await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+
+  logger.info(`Server is running on http://localhost:${PORT}`);
+  logger.info(`Apollo Server ready at http://localhost:${APOLLO_PORT}`);
+};
+
 startServer();
+
