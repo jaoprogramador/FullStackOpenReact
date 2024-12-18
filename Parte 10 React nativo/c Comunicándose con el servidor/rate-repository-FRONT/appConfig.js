968a4daa9001a2e1b5ba977f9c14c88a95a17e88
jao <jaoprogramador@gmail.com>
2024-12-18 12:28:09 +0100

    Guardar cambios locales

--- Parte 10 React nativo/c Comunicándose con el servidor/rate-repository-FRONT/appConfig.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-const AppConfig = {
-    APOLLO_URI: process.env.REACT_APP_APOLLO_URI,
-    ENV: process.env.REACT_APP_APOLLO_ENV,
-  };
-  
-  export default AppConfig;
-  
--- Parte 10 React nativo/c Comunicándose con el servidor/rate-repository-FRONT_MAL/src/graphql/mutations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const AUTHORIZE = gql`
-  mutation Authorize($credentials: AuthorizeInput!) {
-    authorize(credentials: $credentials) {
-      accessToken
-    }
-  }
-`;
